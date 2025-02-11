975856a852decb327da3662325f68a72aedcb4e8
agnes31 <agnesomet@gmail.com>
2025-02-11 12:56:34 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const app = express()
+const corsOption = {
+    origin: 'http://localhost:8080',
+    credentials:true,
+}
+app.use(cors(corsOption))
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended:true }))
+//test
+app.get('/', (req, res) => {
+    res.json({message: 'Welcome'})
+})
+
+const PORT =  8080
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}.`)
+})
