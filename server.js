a9f45b0c723f0eb9c3250163760489cc101c0355
agnes31 <agnesomet@gmail.com>
2025-02-11 14:52:29 +0700

    Creating the router

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ app.use(bodyParser.urlencoded({ extended:true }))
 app.get('/', (req, res) => {
     res.json({message: 'Welcome'})
 })
+require('./app/routes/product.route')(app)
 
 const db = require('./app/models')
 db.connex.sync()
